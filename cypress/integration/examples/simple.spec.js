ec7a02567bef373059121d56b3fa94306aa8cf5a
=Seba <=omgl0lwth@gmail.com>
2020-08-30 10:23:45 +0200

    another describe block, run headless mode, videos

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,5 +1,16 @@
 describe('My first test', () => {
     it('True should be true', () => {
-        expect(true).to.equal(true);
+        expect(true).to.equal(true)
+    });
+
+    it('5 should be 5', () => {
+        expect(5).to.equal(5)
+  
+    });
+});
+
+describe('Another describe block', () => {
+    it('False should be false', () => {
+        expect(false).to.equal(false);
     });
 });
