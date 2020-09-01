eeea9423ea08d2f8c3fb34950427fd346b794b8e
=Seba <=omgl0lwth@gmail.com>
2020-09-01 12:20:25 +0200

    interacting with buttons

---
+++ cypress/integration/examples/buttons.spec.js
@@ -0,0 +1,15 @@
+describe('Browser actions', () => {
+    it('should load books website', () => {
+        cy.visit('http://books.toscrape.com/index.html', { timeout: 6000 })
+        cy.url().should('include', 'index.html')
+    });
+
+    it('should click on travel category', () => {
+
+        cy.get('a')
+        .contains('Travel')
+        .click();
+        
+        cy.get('h1').contains('Travel');
+    });
+});
