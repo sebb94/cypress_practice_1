22ac827b50641120b21c1b1ccf704d743fc9cd22
sebb94 <omgl0lwth@gmail.com>
2020-09-03 21:09:11 +0200

    login by fixtures from json

---
+++ cypress/integration/examples/fixtures.spec.js
@@ -0,0 +1,17 @@
+describe('Login with fixtures', () => {
+
+    it('should try to login', () => {
+        cy.visit('zero.webappsecurity.com/login.html')
+
+        cy.fixture('user').then(user => {
+            const username = user.username
+            const password = user.password
+
+            cy.get('#user_login').type(username)
+            cy.get("#user_password").type(password)
+            cy.contains('Sign in').click()
+        })
+
+
+    });
+});
