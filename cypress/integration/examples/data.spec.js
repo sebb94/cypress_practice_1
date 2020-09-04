810ccbd9a5f8bb44ef65fbe13014221cf222b30a
sebb94 <omgl0lwth@gmail.com>
2020-09-04 22:13:58 +0200

    read from json and txt file

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -7,4 +7,12 @@ describe('Write / Read data to Json / Text file', () => {
     it('should write data into txt', () => {
         cy.writeFile('log.txt', 'Hello World')
     });
+
+    it('should read and verify json file', () => {
+        cy.readFile('log.json').its('age').should('eq',25)
+    });
+
+    it('should read and verify txt file', () => {
+        cy.readFile('log.txt').should('eq','Hello World')
+    });
 });
