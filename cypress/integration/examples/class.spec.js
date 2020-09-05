4768175d25b58e775890d2cd974848778665bb06
sebb94 <omgl0lwth@gmail.com>
2020-09-05 10:02:06 +0200

    hooks - before, after, beforeEach, afterEach, only, skip

--- cypress/integration/examples/class.spec.js
+++ cypress/integration/examples/class.spec.js
@@ -19,12 +19,51 @@ class HomePage extends BasePage {
 }
 
 describe('Abstraction with classes', () => {
-    it('Should scroll down and scroll up on the page', () => {
+    before(() => {
+        console.info('SetUp')
         HomePage.loadHomePage()
+    });
+
+    after(() => {
+        console.info("After")
+    });
+
+    beforeEach(() => {
+        // runs before each it block describe
+        console.info("Before each")
+    });
+
+    afterEach(() => {
+        // runs after each it block in describe
+        console.info("After each")
+    });
+
+    it.skip('1st Should scroll down and scroll up on the page', () => {
         HomePage.wait(2000)
         HomePage.scrollDown()
         HomePage.wait(2000)
         HomePage.scrollUp()
+    });
 
+
+    it('2nd Should scroll down and scroll up on the page', () => {
+        HomePage.wait(2000)
+        HomePage.scrollDown()
+        HomePage.wait(2000)
+        HomePage.scrollUp()
+    });
+
+    it('3rd Should scroll down and scroll up on the page', () => {
+        HomePage.wait(2000)
+        HomePage.scrollDown()
+        HomePage.wait(2000)
+        HomePage.scrollUp()
+    });
+
+    it.only('4th Should scroll down and scroll up on the page', () => {
+        HomePage.wait(2000)
+        HomePage.scrollDown()
+        HomePage.wait(2000)
+        HomePage.scrollUp()
     });
 });
