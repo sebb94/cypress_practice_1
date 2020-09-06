2b91d50290d6ffb39fc05f8180dc637e92177c02
sebb94 <omgl0lwth@gmail.com>
2020-09-06 09:59:01 +0200

    e2e - feedback form

---
+++ cypress/integration/e2e/feedback.spec.js
@@ -0,0 +1,38 @@
+describe('Seachbox test', () => {
+    before(() => {
+        cy.visit("http://zero.webappsecurity.com/")
+        cy.contains('Feedback').click()
+        cy.url().should('include', 'feedback.html')
+    });
+
+    it('should load feedback form', () => {
+        cy.get('form[action="/sendFeedback.html"')
+            .should('be.visible')
+    });
+
+    it('should fill name', () => {
+        cy.get('input#name').type('Seba')
+    });
+
+    it('should fill email', () => {
+            cy.get('input#email').type('seba@gmail.com')
+    });
+
+    it('should fill subject', () => {
+        cy.get('input#subject').type('Subdżekt')
+    });
+
+    it('should fill message', () => {
+        cy.get('textarea#comment').type('Mesedż')
+    });
+
+    it('should send message ', () => {
+        cy.contains('Send Message').click()
+    });
+
+    it('should show thank you message', () => {
+        cy.get('.top_offset').should('include.text','Thank you for your comments')
+    });
+
+
+});
