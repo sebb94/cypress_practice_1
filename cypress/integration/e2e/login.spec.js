b7dd5ebf8a0d58f8c61d5dc884af25a7aa8b457c
sebb94 <omgl0lwth@gmail.com>
2020-09-07 18:36:31 +0200

    login / logout test case

---
+++ cypress/integration/e2e/login.spec.js
@@ -0,0 +1,49 @@
+describe('Login test', () => {
+
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.url().should('include','index.html')
+    });
+
+    it('login with incorrect credentials', () => {
+        cy.contains('Signin').click()
+        cy.fixture('user').then(user => {
+            const username = user.invalid_username
+            const password = user.invalid_password
+
+            cy.get('#user_login').type(username)
+            cy.get("#user_password").type(password)
+            cy.contains('Sign in').click()
+        })
+    });
+
+    it('validate error message', () => {
+        cy.get('.alert-error').contains('are wrong.')
+    });
+
+    it('login with correct credentials', () => {
+        cy.fixture('user').then(user => {
+            const username = user.valid_username
+            const password = user.valid_password
+
+            cy.get('#user_login').type(username)
+            cy.get("#user_password").type(password)
+            cy.contains('Sign in').click()
+        })
+    });
+
+    it('validate profile page', () => {
+        cy.get('.icon-user').should('be.visible')
+        cy.get('h2.board-header').contains('Cash Accounts')
+    });
+
+    it('logout', () => {
+         cy.get('.icon-user').click()
+         cy.get("#logout_link").click()
+
+    });
+
+    it('validate logout', () => {
+        cy.get('#homeMenu').should('have.class','active')
+    });
+});
